645e3ff1758355f14c7db9a58ea89f8d36b6b82a
Xakker04 <157778986+Xakker04@users.noreply.github.com>
2026-01-12 12:11:12 +0500

    Update script.js

--- script.js
+++ script.js
@@ -27,4 +27,19 @@ const navMenu = document.getElementById('nav-menu');
 navToggle.addEventListener('click', () => {
     navMenu.classList.toggle('active'); // CSS bilan mos
 });
+// ACCORDION
+document.querySelectorAll(".accordion-header").forEach(header => {
+    header.addEventListener("click", () => {
+        const item = header.parentElement;
+        item.classList.toggle("active");
+
+        const content = item.querySelector(".accordion-content");
+        if (item.classList.contains("active")) {
+            content.style.maxHeight = content.scrollHeight + "px";
+        } else {
+            content.style.maxHeight = null;
+        }
+    });
+});
+
 
